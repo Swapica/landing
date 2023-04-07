7cd0e8f34a6a710ca3bd948d133e4f0ef80dfb7a
electr1xx <aleksandrplotnikov1997@gmail.com>
2023-04-07 13:22:34 +0300

    wip

--- src/pages/Terms.vue
+++ src/pages/Terms.vue
@@ -27,6 +27,39 @@
       <p class="terms__paragraph">
         {{ $t('terms.block-1-desc-part-4') }}
       </p>
+
+      <h4 class="terms__subblock-title">
+        {{ $t('terms.block-1-subblock-1-title') }}
+      </h4>
+      <p class="terms__paragraph">
+        {{ $t('terms.block-1-subblock-1-desc') }}
+      </p>
+
+      <h4 class="terms__subblock-title">
+        {{ $t('terms.block-1-subblock-2-title') }}
+      </h4>
+      <p class="terms__paragraph">
+        {{ $t('terms.block-1-subblock-2-desc') }}
+      </p>
+    </div>
+
+    <div class="terms__block">
+      <h3>{{ $t('terms.block-2-title') }}</h3>
+
+      <h4 class="terms__subblock-title">
+        {{ $t('terms.block-2-subblock-1-title') }}
+      </h4>
+      <p
+        class="terms__paragraph"
+        v-html="$t('terms.block-2-subblock-1-desc')"
+      />
+
+      <h4 class="terms__subblock-title">
+        {{ $t('terms.block-2-subblock-2-title') }}
+      </h4>
+      <p class="terms__paragraph">
+        {{ $t('terms.block-2-subblock-2-desc') }}
+      </p>
     </div>
   </div>
 </template>
@@ -46,6 +79,13 @@
   margin-top: toRem(16);
 
   b {
+    color: var(--text-primary-dark);
+  }
+
+  a {
+    font-size: inherit;
+    text-decoration: underline;
+    line-height: inherit;
     color: var(--text-primary-dark);
   }
 
@@ -79,5 +119,13 @@
   @include respond-to(tablet) {
     font-size: toRem(18);
   }
+}
+
+.terms__subblock-title {
+  margin-top: toRem(16);
+}
+
+.terms__block {
+  margin-bottom: toRem(40);
 }
 </style>
